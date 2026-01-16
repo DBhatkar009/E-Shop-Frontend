c2d5336833db064a3b248655a74f2c86bc7b90e0
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2026-01-16 18:35:45 +0530

    prefix change e-shop-frontend to admin in eslintrc.json & generated dashboard component on pages.

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
     standalone: true,
     imports: [ RouterModule],
+    // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'e-shop-frontend-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
