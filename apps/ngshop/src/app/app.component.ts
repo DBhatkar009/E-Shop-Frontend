3c3e8adc4cca6b32971f531faabe2c9eed3d83cd
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2026-01-16 15:44:09 +0530

    generated footer & header component on shared folder

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   standalone: true,
-  imports: [ RouterModule],
+  imports: [RouterModule, HomePageComponent, ProductListComponent, FooterComponent, HeaderComponent],
   selector: 'e-shop-frontend-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
